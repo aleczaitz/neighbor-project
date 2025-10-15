1ff5451dcdfdb18fc9a0e2127042fc4b134e5c66
Alec Zaitz <aleczaitz@gmail.com>
2025-10-15 16:16:36 -0600

    setup aws eb hosting

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
+
+// Post endpoint to handle incoming JSON data
+app.post('/', (req, res) => {
+    const data = req.body;
+
+    res.json(data);
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {console.log('Server is running on port ', PORT)});
